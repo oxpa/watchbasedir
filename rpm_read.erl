cd2d65d858a8bd6687e9c039c2a17972445b379f
Ippolitov A. Igor <iippolitov@gmail.com>
2014-02-25 22:35:36 +0400

    evening commit

--- rpm_read.erl
+++ rpm_read.erl
@@ -10,14 +10,14 @@
 
 %% A helper function for C strings reading.
 %% It's almost for sure have to be rewritten
-read_till_zero(F) -> read_till_zero(F, 1 ). 
-read_till_zero(F, Count) -> read_till_zero(F, Count, [], 0, <<>>).
-read_till_zero(_, 0, Strings, Offset , _ ) -> {ok, {Offset, lists:reverse(Strings)}};
-read_till_zero(F, Count, Strings, Offset , Acc) ->
+read_till_zero(F, Max_Offset) -> read_till_zero(F, 1, Max_Offset ). 
+read_till_zero(F, Count, Max_Offset) -> read_till_zero(F, Count, Max_Offset, [], 0, <<>>).
+read_till_zero(_, 0, Max_Offset, Strings, Offset , _ ) -> {ok, {Offset, lists:reverse(Strings)}};
+read_till_zero(F, Count, Max_Offset, Strings, Offset , Acc) when Offset < Max_Offset ->
 	case file:read(F,1) of
-		{ok, <<0>>} -> read_till_zero(F, Count - 1, [binary:bin_to_list(Acc)|Strings], Offset + 1, <<>>);
+		{ok, <<0>>} -> read_till_zero(F, Count - 1, Max_Offset, [binary:bin_to_list(Acc)|Strings], Offset + 1, <<>>);
 		{ok, Data} when is_binary(Data) -> 
-			           read_till_zero(F, Count, Strings, Offset + 1, <<Acc/binary,Data/binary>> )
+			           read_till_zero(F, Count, Max_Offset,  Strings, Offset + 1, <<Acc/binary,Data/binary>> )
 	end.
 
 %% One more helper function due to rpm header structure
@@ -26,7 +26,7 @@ round_offset_by_eight(F,Off) ->
 
 %% A helper for future integration with lager or other logger
 debug(Message,Args) ->
-	%io:format(Message,Args),
+	io:format(Message,Args),
 	true.
 
 %% A function used in shell to test module.
@@ -37,12 +37,12 @@ on_load(File) ->
 read_rpm(RPM) ->
 		{ok,F} = file:open(RPM,[read, binary, read_ahead]),
 		{ok, Lead} = read_rpm_lead(F),
-		{ok, Indexes} = read_rpm_signature_header(F),
-		{ok, {Off,Signature}} = read_rpm_header_data(F,Indexes),
+		{ok, {header_length, Max_Offset},Indexes} = read_rpm_signature_header(F),
+		{ok, {Off,Signature}} = read_rpm_header_data(F,Indexes, Max_Offset),
 		%% signature may end anywhere in the file. We should round the offset by 8 byte boundary
 		round_offset_by_eight(F,Off),
-		{ok, Head_indexes} = read_rpm_signature_header(F),
-		{ok, {_,Header}} = read_rpm_header_data(F,Head_indexes),
+		{ok,{header_length, Max_Offset}, Head_indexes} = read_rpm_signature_header(F),
+		{ok, {_,Header}} = read_rpm_header_data(F,Head_indexes, Max_Offset),
 		#rpm{filename=RPM, lead=Lead, signature=Signature, header=Header}.
 		
 read_rpm_lead(F) ->
@@ -63,12 +63,12 @@ read_rpm_signature_header(F) ->
 	% 9350632 = 16#8ead e8 - header magic number
 	Magic = 9350632,
 	debug("Read magic: ~.16B , will read ~B index records with  ~B bytes of signature~n",[Magic,Index_count,Signature_bytes]),
-	read_rpm_index_entry(F,Index_count).
+	read_rpm_index_entry(F,Index_count, Signature_bytes).
 	
-read_rpm_index_entry(F, N) 	-> read_rpm_index_entry(F,N, []).
-read_rpm_index_entry(_, 0, Acc) ->
+read_rpm_index_entry(F, N, Max_Offset) 	-> read_rpm_index_entry(F,N, Max_Offset, []).
+read_rpm_index_entry(_, 0, Max_Offset, Acc) ->
 	%% return sorted by offset index list from Acc 
-	{ok, 
+	{ok, {header_length, Max_Offset},
 	 lists:sort(
 		fun(A,B) when 
 			A#rpm_sig_index.offset =< B#rpm_sig_index.offset	-> true; 
@@ -76,7 +76,7 @@ read_rpm_index_entry(_, 0, Acc) ->
 		end
 		, Acc)
 	};
-read_rpm_index_entry(F, Num_of_indexes, Acc) when Num_of_indexes > 0 -> 
+read_rpm_index_entry(F, Num_of_indexes, Max_Offset, Acc) when Num_of_indexes > 0 -> 
 	%% index is 4 fields of 4 bytes. Read them recursevly. Then proceed to data reading
     Index_length=16,
     {ok, Data} = file:read(F,Index_length),
@@ -85,35 +85,43 @@ read_rpm_index_entry(F, Num_of_indexes, Acc) when Num_of_indexes > 0 ->
 	read_rpm_index_entry(F, Num_of_indexes - 1, [#rpm_sig_index{tag=Tag,data_type=Data_type, offset=Offset,num_of_entries=Num_of_entries}|Acc]).
 	
 
-read_rpm_header_data(F, Indexes) -> read_rpm_tag_data(F, Indexes, {0, []}).
+read_rpm_header_data(F, Indexes, Max_Offset) -> read_rpm_tag_data(F, Indexes, Max_Offset, {0, []}).
 
 
 % reading ended. Return.
-read_rpm_tag_data(_, [], {Off, Data}) -> {ok, {Off,Data}};
+read_rpm_tag_data(_, [], _, {Off, Data}) -> {ok, {Off,Data}};
 
 % read some data till next tag boundary
-read_rpm_tag_data(F, [Index|Tail], {Off,Data}) when Index#rpm_sig_index.offset - Off /= 0 -> 
+read_rpm_tag_data(F, [Index|Tail], Max_Offset, {Off,Data}) when Index#rpm_sig_index.offset - Off /= 0, Off =< Max_Offset -> 
 	debug("Skipping through from ~B to ~B ~n",[Off, Index#rpm_sig_index.offset]),
 	{ok, _} = file:read(F,Index#rpm_sig_index.offset - Off),
-	read_rpm_tag_data(F, [Index|Tail], {Index#rpm_sig_index.offset,Data});
+	read_rpm_tag_data(F, [Index|Tail], Max_Offset, {Index#rpm_sig_index.offset,Data});
 
 % going to read a 6-th data type - a string
 % or a list of string (8 and 9). 7 is binary and is read the other way
-read_rpm_tag_data(F, [Index|Tail], {Off, Data}) when Index#rpm_sig_index.data_type == 6; Index#rpm_sig_index.data_type > 7 ->
-	{ok,{Offset,String}} =  read_till_zero(F, Index#rpm_sig_index.num_of_entries),
+read_rpm_tag_data(F, [Index|Tail], Max_Offset, {Off, Data}) when Index#rpm_sig_index.data_type == 6; Index#rpm_sig_index.data_type > 7 ->
+	{ok,{Offset,String}} =  read_till_zero(F, Index#rpm_sig_index.num_of_entries, Max_Offset ),
 	debug("Read ~B bytes starting from ~B ~n",[Offset,Off]),
 	Tag_data = {Index#rpm_sig_index.tag, String},
-	read_rpm_tag_data(F, Tail, {Offset+Off,[Tag_data|Data]});
+	read_rpm_tag_data(F, Tail, Max_Offset, {Offset+Off,[Tag_data|Data]});
 
 % read a "simple" value which size can be calculated using Index information.
-read_rpm_tag_data(F, [Index|Tail], {Off, Data}) when Index#rpm_sig_index.data_type /= 6, Index#rpm_sig_index.data_type < 8  ->
+read_rpm_tag_data(F, [Index|Tail], Max_Offset, {Off, Data}) when Index#rpm_sig_index.data_type /= 6, Index#rpm_sig_index.data_type < 8  ->
 	%Off=Index#rpm_sig_index.offset,
 	Multiplier = proplists:get_value(Index#rpm_sig_index.data_type, rpm_types_sizes()),
 	{ok, D} = file:read(F, Index#rpm_sig_index.num_of_entries * Multiplier), 
 	debug("read ~B bytes by ~B bytes, starting from ~B~n",[Index#rpm_sig_index.num_of_entries * Multiplier, Multiplier, Off]),
 	Tag_data = {Index#rpm_sig_index.tag, [ Bin || <<Bin:Multiplier/unit:8>> <= D ]},
 	debug("~w, ~w~n",[D, Tag_data]),
-	read_rpm_tag_data(F, Tail, {Off + Index#rpm_sig_index.num_of_entries * Multiplier, [Tag_data|Data]}).
+	read_rpm_tag_data(F, Tail, Max_Offset, {Off + Index#rpm_sig_index.num_of_entries * Multiplier, [Tag_data|Data]}).
+
+%% filelist is in 3 indexes: basenames, directories and "a link" between them, dir index.
+%% length (Dir_index) == length( Base_names)
+%% directories are numerated from 0, so have to add 1 in nth/2
+join_filelist(Dir_index, Base_names, Directories) when Dir_index == undefined; Base_names == undefined; Directories == undefined -> 
+    {'file',''};
+join_filelist(Dir_index, Base_names, Directories) -> 
+        lists:zipwith(fun(A,B)-> {'file', lists:nth(A+1, Directories) ++ B} end, Dir_index,  Base_names).
 
 %%% Functions to work with rpm description from read_rpm/1
 rpm_get_filelist(RPM_DESC) ->
@@ -121,21 +129,9 @@ rpm_get_filelist(RPM_DESC) ->
 				proplists:get_value(1117, RPM_DESC#rpm.header),
 				proplists:get_value(1118, RPM_DESC#rpm.header)).
 
-%% filelist is in 3 indexes: basenames, directories and "a link" between them, dir index.
-%% length (Dir_index) == length( Base_names)
-%% directories 
-join_filelist(Dir_index, Base_names, Directories) -> %when is_binary(Dir_indexes) ->
-	%lists:map(fun(A) -> io:format("[~s],~n",[A]) end,Base_names ++ Directories),
-	%io:format("~w~n ~s~n ~s~n", [Dir_index, Base_names, Directories]),
-	lists:map(fun({A,B})-> A++B end, 
-			  lists:keymap( fun(Index)->lists:nth(Index + 1, Directories) end, 
-							1, 
-							lists:zip((Dir_index), Base_names)
-			  )
-	).
-	
-
-
-
-
-
+%rpm_get_name(RPM_DESC)-> {}
+%rpm_get_arch(RPM_DESC)-> {}
+%rpm_get_version(RPM_DESC) -> %{version, [epoch, ver, release]}
+%rpm_get_checksum(RPM_DESC) -> %{checksum, {type, pkgid}, checksum}
+%rpm_get_summary() ->
+%rpm_get_description() ->
