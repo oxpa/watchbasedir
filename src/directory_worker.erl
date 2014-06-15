7b5bdf971ebc51f417671262cc6f17bcb2f8d0bb
oxpa <iippolitov@gmail.com>
2014-06-15 23:51:18 +0400

    evening commit

---
+++ src/directory_worker.erl
@@ -0,0 +1,40 @@
+-module(directory_worker).
+-compile(export_all).
+-behavior(gen_server).
+-export([init/1,terminate/2,handle_cast/2,handle_info/2, handle_call/3, code_change/3]).
+
+-record(state, {base, table, descriptors, waiting, options}).
+
+start_link(Args) ->
+    gen_server:start_link(?MODULE, Args, []).
+
+init(Args) -> gen_server:cast({start}), {ok,#state.options=Args}.
+
+handle_call(_Request, _From, State) -> {reply,ok,State}.
+handle_cast({start},State) -> Dir=proplists:get_value(directory,State#state.options), 
+							  preprocess(Dir), 
+							  start_watch(Dir);
+handle_cast(_Msg, State) -> {noreply, State}.
+handle_info(_Info, State) -> {noreply, State}.
+terminate(_Reason, _State) -> ok.
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+
+start_watch(Directory) ->
+	lists:foreach(fun(A) -> inoteefy:watch(A,fun inotify_callback/1) end, subdirs(Directory)).
+
+preprocess(Directory) -> ok.
+inotify_callback({Dir_Name, Operation,Cookie, Name}) -> ok.
+
+
+subdirs(Directory) -> subdirs(Directory, []).
+subdirs(Directory, Accumulator) ->
+	io:format("processing ~s: ", [Directory]),
+	case file:list_dir(Directory) of 
+		{ok, List} -> lists:foldl(fun subdirs/2,[Directory|Accumulator], lists:filtermap(fun("repodata") -> false; 
+																							(A) -> {true,filename:join(Directory,A)} end,
+																						List));
+		{error, _Reason} -> Accumulator
+	end.
+
+
