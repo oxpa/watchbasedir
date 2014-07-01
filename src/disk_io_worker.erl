cbd7d85f206f9e0816f1b80fb15cdbbb7acf7a20
oxpa <iippolitov@gmail.com>
2014-07-01 21:03:15 +0400

    adding better logging into disk_io_worker module

--- src/disk_io_worker.erl
+++ src/disk_io_worker.erl
@@ -12,6 +12,8 @@ start_link(_) ->
 	gen_server:start_link(?MODULE, [], []).
 
 init([]) -> 
+	lager:info("starting worker"),
+	process_flag(trap_exit, true),%to finish any file processin that might be in process
 	{ok,[]}.
  
 stop(_) -> gen_server:call(?MODULE, stop).
@@ -19,41 +21,43 @@ stop(_) -> gen_server:call(?MODULE, stop).
 
 % call to cache a file
 handle_call({process_file,Dir, F},_From, S) ->
-	error_logger:info_report("this worker is going to process "++F),
+	lager:info("Worker is going to cache ~r",[F]),
 	rpmr:cache_package(Dir,F),	
 	case supervisor:start_child(scout, {Dir, {scout, start_link,[Dir]}, transient, brutal_kill, worker, [scout]}) of
 		{ok,_} -> ok;
-        {error,{already_started,P} } -> error_logger:info_report("casting message to "++io_lib:format("~p",[P])),
-                                        gen_server:cast(P,Dir);
-        _ -> ok
+        {error,{already_started,P} } -> gen_server:cast(P,Dir);
+        _ -> lager:critical("Unable to start scout for ~r",[Dir])
     end,
-
-	{reply,ok, S};
+	{reply, ok, S};
 
 % call to build repo from cache
 handle_call({build_dir,D},_From, S) ->
-	error_logger:info_report("this worker is going to build repo from cache in "++D),
+	lager:info("Worker is going to build repo from cache at ~r",[D]),
 	rpmr:generate_repo(cached, D),
     {reply,ok, S};
 
 % call to process through all rpm files in a dir and verify cache
 handle_call({process_dir,D},_From, S) ->
-	error_logger:info_report("this worker is going to look through all packages in "++D),
+	lager:info("Worker is going to cache all packages in ~r",[D]),
 	rpmr:generate_repo(direct, D),
 	{reply, ok, S};
 
 % handle a request to stop
-handle_call(stop, _From, S) -> {stop, "Call", ok, S};
+handle_call(stop, From, S) -> 
+	lager:info("Worker is stopping by request from ~r", [From]),
+	{stop, "Call", ok, S};
 
 % a generic call handler. Caller should crash after sending this call
-handle_call(Message, _From, S) ->
-	error_logger:info_report("got call "++io_lib:format("~p",[Message])),
+handle_call(Message, From, S) ->
+	lager:error("Worker got unexpected message ~r from ~r",[Message, From]),
 	{noreply, S}.
 
 handle_cast(M, S) ->
-	error_logger:info_report("got cast "++io_lib:format("~p",[M])),
-	{noreply, S, ?DELAY}.
+	lager:error("Worker got unexpected message ~r",[M]),
+	{noreply, S}.
 
-handle_info(_,_) -> ok.
-code_change(_,_,_) -> ok.
+handle_info(I,S) -> 
+	lager:error("Worker gor unexpected information ~r",[I]),
+	{noreply,S}.
+code_change(_,_,_) -> {stop,"No code upgrade supposed for worker"}.
 terminate(_,_) -> ok.
