57ec4464c61c861688216157d59dd01add534fa2
oxpa <iippolitov@gmail.com>
2014-07-02 00:58:33 +0400

    more logging

--- src/disk_io_worker.erl
+++ src/disk_io_worker.erl
@@ -12,7 +12,7 @@ start_link(_) ->
 	gen_server:start_link(?MODULE, [], []).
 
 init([]) -> 
-	lager:info("starting worker"),
+	lager:info("starting worker ~p", [self()]),
 	process_flag(trap_exit, true),%to finish any file processin that might be in process
 	{ok,[]}.
  
@@ -21,43 +21,46 @@ stop(_) -> gen_server:call(?MODULE, stop).
 
 % call to cache a file
 handle_call({process_file,Dir, F},_From, S) ->
-	lager:info("Worker is going to cache ~r",[F]),
+	lager:info("Worker ~p is going to cache ~p",[self(),F]),
 	rpmr:cache_package(Dir,F),	
 	case supervisor:start_child(scout, {Dir, {scout, start_link,[Dir]}, transient, brutal_kill, worker, [scout]}) of
 		{ok,_} -> ok;
         {error,{already_started,P} } -> gen_server:cast(P,Dir);
-        _ -> lager:critical("Unable to start scout for ~r",[Dir])
+        _ -> lager:critical("Unable to start scout for ~p",[Dir])
     end,
 	{reply, ok, S};
 
 % call to build repo from cache
 handle_call({build_dir,D},_From, S) ->
-	lager:info("Worker is going to build repo from cache at ~r",[D]),
+	lager:info("Worker is going to build repo from cache at ~p",[D]),
 	rpmr:generate_repo(cached, D),
     {reply,ok, S};
 
 % call to process through all rpm files in a dir and verify cache
 handle_call({process_dir,D},_From, S) ->
-	lager:info("Worker is going to cache all packages in ~r",[D]),
+	lager:info("Worker ~p is going to cache all packages in ~p",[self(),D]),
 	rpmr:generate_repo(direct, D),
 	{reply, ok, S};
 
 % handle a request to stop
 handle_call(stop, From, S) -> 
-	lager:info("Worker is stopping by request from ~r", [From]),
+	lager:info("Worker ~p is stopping by request from ~p", [self(),From]),
 	{stop, "Call", ok, S};
 
 % a generic call handler. Caller should crash after sending this call
 handle_call(Message, From, S) ->
-	lager:error("Worker got unexpected message ~r from ~r",[Message, From]),
+	lager:error("Worker ~p got unexpected message ~p from ~p",[self(),Message, From]),
 	{noreply, S}.
 
 handle_cast(M, S) ->
-	lager:error("Worker got unexpected message ~r",[M]),
+	lager:error("Worker ~p got unexpected message ~p",[self(),M]),
 	{noreply, S}.
 
+handle_info({'EXIT',_,shutdown},S) -> 
+	lager:debug("Worker ~p got EXIT and actually quiting",[self()]),
+	{stop,shutdown,S};
 handle_info(I,S) -> 
-	lager:error("Worker gor unexpected information ~r",[I]),
+	lager:error("Worker got unexpected information ~p",[I]),
 	{noreply,S}.
 code_change(_,_,_) -> {stop,"No code upgrade supposed for worker"}.
-terminate(_,_) -> ok.
+terminate(_,_) -> lager:debug("Worker ~p teminating",[self()]),ok.
--- src/scout.erl
+++ src/scout.erl
@@ -36,28 +36,28 @@ stop(_) ->
 
 % someone called us and should die it's own way later
 handle_call(M,F,State) -> 
-	lager:error("Scout got message ~r from ~r, but it shouldn't!", [M,F]),
+	lager:error("Scout got message ~p from ~p, but it shouldn't!", [M,F]),
 	{noreply, State, ?DELAY}.
 
 % delay file or directory processing if they were changed within configured delay
 handle_cast(Message, State) when State#state.dir==Message -> 
-	lager:debug("Scout got request to build ~r  while already waiting for it", [Message]),
+	lager:debug("Scout got request to build ~p  while already waiting for it", [Message]),
 	{noreply, State, ?DELAY}; 
 handle_cast({Dir,_Type,_OP,_Coockie,Name}, State) when State#state.name == Name, State#state.dir == Dir ->
-	lager:debug("Scout got request to cache ~r ,while already waiting for it", [Name]),
+	lager:debug("Scout got request to cache ~p ,while already waiting for it", [Name]),
 	{noreply, State, ?DELAY};
 handle_cast(Message, State) -> 
-	lager:warning("Scout got ~r while waiting for ~r~r", [Message,State#state.dir,State#state.name]), 
+	lager:warning("Scout got ~p while waiting for ~p~p", [Message,State#state.dir,State#state.name]), 
 	{noreply, State, ?DELAY}. 
 
 
 handle_info(timeout,State=#state{name=""}) -> 
-	lager:info("Scout calling poolboy to build repo in ~r", [State#state.dir]),
+	lager:info("Scout calling poolboy to build repo in ~p", [State#state.dir]),
     poolboy:transaction(disk_workers, fun(Worker) -> gen_server:call(Worker,{build_dir,State#state.dir}) end, 60000),
     {stop,shutdown,State};
 
 handle_info(timeout,State) when State#state.name /= "" , State#state.dir /= "" -> 
-	lager:debug("Scout calling poolboy to cache ~r ~r",[State#state.dir,State#state.name]),
+	lager:debug("Scout calling poolboy to cache ~p ~p",[State#state.dir,State#state.name]),
 	poolboy:transaction(disk_workers, fun(Worker) -> gen_server:call(Worker,{process_file,State#state.dir++State#state.name}) end, 60000),
 	{stop,shutdown,State};
 
@@ -65,5 +65,5 @@ handle_info(timeout,State) ->
 	lager:error("Scout got timeout without arguments"),
 	{stop,error,State}.
 
-code_change(_,_,_) -> {error,"No code upgrade supposed"}.
+code_change(_,_,_) -> {error,"No code upgrade supposed for scout"}.
 terminate(_,_) -> ok.
